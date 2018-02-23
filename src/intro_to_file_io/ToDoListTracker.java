3102e9e2d7030ddc4f104260a3441bb4ad290001
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-22 18:29:17 -0800

    ToDoListTracker IP
    Finished the save and adding task options.

--- src/intro_to_file_io/ToDoListTracker.java
+++ src/intro_to_file_io/ToDoListTracker.java
@@ -2,16 +2,17 @@ package intro_to_file_io;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class ToDoListTracker implements ActionListener {
 
@@ -21,30 +22,31 @@ public class ToDoListTracker implements ActionListener {
 	JButton removeTask = new JButton();
 	JButton save = new JButton();
 	JButton load = new JButton();
+	JTextArea taskList = new JTextArea();
+
+	ArrayList<String> list = new ArrayList<String>();
 
 	public static void main(String[] args) {
 		ToDoListTracker ToDoListTracker = new ToDoListTracker();
+
 	}
 
 	ToDoListTracker() {
 
 		frame.setVisible(true);
-		panel.setVisible(true);
-		addTask.setVisible(true);
-		removeTask.setVisible(true);
-		load.setVisible(true);
-		save.setVisible(true);
 
 		frame.add(panel);
 		panel.add(addTask);
 		panel.add(removeTask);
 		panel.add(load);
 		panel.add(save);
+		panel.add(taskList);
 
 		addTask.setText("addTask");
 		removeTask.setText("removeTask");
 		load.setText("load");
 		save.setText("save");
+		taskList.setText("List");
 
 		addTask.addActionListener(this);
 		removeTask.addActionListener(this);
@@ -54,31 +56,76 @@ public class ToDoListTracker implements ActionListener {
 		frame.pack();
 	}
 
+	void updateTaskList() {
+
+		String allTask = "";
+
+		for (String task : list) {
+
+			allTask += task;
+			
+			allTask += "\n";
+			
+		}
+
+		taskList.setText(allTask);
+
+		frame.pack();
+		frame.repaint();
+
+	}
+	
+	/*void JFileChooser(){
+		
+	
+	JFileChooser jfc = new JFileChooser();
+	int returnVal = jfc.showOpenDialog(null);
+	if (returnVal == JFileChooser.APPROVE_OPTION) {
+		String fileName = jfc.getSelectedFile().getAbsolutePath();
+		System.out.println(fileName);
+	}
+	}*/
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		// look at the whiteboard picture in photos
 
-		
-		
-		//look at the whiteboard picture from the white board
-		
-		if (e.getSource().equals(save)) {
+		if (e.getSource().equals(addTask)) {
 
-			String taskAdded = JOptionPane.showInputDialog("Task");
+			String taskAdded = JOptionPane.showInputDialog(" Add a task: ");
 
-			try {
-				FileWriter fw = new FileWriter("src/intro_to_file_io/test4.txt", true);
+			list.add(taskAdded);
 
-				fw.write(taskAdded + "\n");
+			updateTaskList();
 
-				fw.close();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
+			System.out.println(taskAdded);
 
 		}
 
-		if (e.getSource().equals(removeTask)) {
+		if (e.getSource().equals(save)) {
+			try {
+				JFileChooser jfc = new JFileChooser() ;
+				int returnVal = jfc.showOpenDialog(null);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					String fileName = jfc.getSelectedFile().getAbsolutePath();
+					System.out.println(fileName);
+				}
+
+			FileWriter fw = new FileWriter(jfc.getSelectedFile(), true);
+		
+
+			for (String task : list) {
+
+				fw.write(task + "\n");
+			}
+			
+			
+
+				fw.close();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
 
 		}
 
